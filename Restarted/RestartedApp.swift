27da22e62d78fcbdb8c9a67e93933a3ce330351d
Daniil Krivonogov <113389092+metalWillHelpYou@users.noreply.github.com>
2024-05-24 11:46:10 +0200

    Initial Commit

---
+++ Restarted/RestartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartedApp.swift
+//  Restarted
+//
+//  Created by metalWillHelpYou on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
