73b33d894927eb63b5124672d835ac575fd20d1a
Daniil Krivonogov <113389092+metalWillHelpYou@users.noreply.github.com>
2024-05-29 18:59:45 +0200

    fix article card

--- Restarted/Views/ArticleViews/ArticleView.swift
+++ Restarted/Views/ArticleViews/ArticleView.swift
@@ -19,22 +19,21 @@ struct ArticleView: View {
             
             Image(article.imageName)
                 .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 368, height: 228)
+                .frame(height: 228)
                 .clipShape(RoundedRectangle(cornerRadius: 15))
                 .shadow(color: .black.opacity(0.4), radius: 4, y: 4)
             
             Text(article.content)
             
             Spacer()
-            
         }
-        .padding(.horizontal)
+        .padding()
+        .themedModifiers()
     }
 }
 
 #Preview {
-    ArticleView(article: Article(title: "What is\nvideo game addiction?", content:"Video game addiction is a behavioral disorder characterized by excessive and com", imageName: "1st"))
+    ArticleView(article: Article(title: "What is video game addiction?", content:"Video game addiction is a behavioral disorder characterized by excessive and com", imageName: "1st"))
 }
 
 //TODO: показать сколько времени потребуется для прочтения
